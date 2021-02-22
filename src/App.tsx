89fde7c1bfe5e98a418b5d801f5ad8ebdeabd089
Jared Gentry <gentryj2@email.sc.edu>
2021-02-21 19:06:21 -0500

    Pull in tailwind css from CDN

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import "./styles.css";
-
 export default function App() {
   return (
-    <div className="App">
+    <div className="text-center bg-purple-300 text-purple-700 text-4xl font-bold">
       <h1>Hello World</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
